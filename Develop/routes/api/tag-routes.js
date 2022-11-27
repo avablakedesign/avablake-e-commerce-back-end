ed91ac232e01d14a214ece44830b731f567d0b6c
Ava Blake <avablakedesign@protonmail.com>
2022-11-27 13:50:41 -0500

    Updated tag-routes.js, routes for PUT /:id and DELETE /:id completed.

--- Develop/routes/api/tag-routes.js
+++ Develop/routes/api/tag-routes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { Tag, Product, ProductTag, Tag } = require('../../models');
+const { Tag, Product, ProductTag} = require('../../models');
 
 // The `/api/tags` endpoint
 
@@ -13,6 +13,7 @@ router.get('/', async (req, res) => {
 router.get('/:id', async (req, res) => {
   const id = req.params.id;
   const tag = await Tag.findByPk({id: Product})
+  res.json(tag);
   // find a single tag by its `id`
   // be sure to include its associated Product data
 });
@@ -24,12 +25,21 @@ router.post('/', async (req, res) => {
   res.json({message:"created new tag"}) 
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a tag's name by its `id` value
+  const id = req.params.id;
+  const updatedTag = req.body;
+  const modifiedTag = await Tag.update(updatedTag, {where:{id}}); 
+  res.json({message: `Updated tag with id ${id}`});
+
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete on tag by its `id` value
+  const id = req.params.id;
+  const destroyedTagRef = await ProductTag.destroy({where:{tag_id: id}});
+  const destroyedTag = await Tag.destroy({where:{id}});
+  res.json({message: `destroyed tag with id ${id}`});
 });
 
 module.exports = router;
